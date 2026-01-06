1ea65c036b58f30e8cebb09df138450c95b5fe6a
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-01-06 20:44:32 +0700

    css in react

---
+++ CSSTrongReact/my-app/src/Cart.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./cart.scss";
+// import { ContainerExtends, StyledButtons } from "./Cart.style";
+import * as Styled from "./Cart.style";
+
+export function Buttons({ isShow, className }) {
+  return (
+    <div className={className + " buttons"}>
+      <button
+        className="button-item"
+        style={{
+          backgroundColor: "yellow",
+          display: isShow ? "inline-block" : "none",
+        }}
+      >
+        hello
+      </button>
+    </div>
+  );
+}
+
+export default function Cart({ isShow }) {
+  return (
+    <Styled.ContainerExtends>
+      Cart
+      <Styled.StyledButtons isShow={isShow} />
+    </Styled.ContainerExtends>
+  );
+}

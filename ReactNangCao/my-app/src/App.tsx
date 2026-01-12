16af321eb7f8e9f1522bb8c4b47ab63b95cc9bbf
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-01-12 20:56:51 +0700

    done project todoList

--- ReactNangCao/my-app/src/App.tsx
+++ ReactNangCao/my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import TodoList from './components/TodoList/TodoList'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
--- ReactNangCao/my-app/src/components/TaskInput/TaskInput.tsx
+++ ReactNangCao/my-app/src/components/TaskInput/TaskInput.tsx
@@ -1,59 +1,60 @@
-// import { useState } from 'react'
-// // import PropTypes from 'prop-types'
-// // import { Todo } from '../../@types/todo.type'
-// import styles from './taskInput.module.scss'
-// // import { TodoTypes } from '../../PropTypes/todo.proptype'
+import { useState } from 'react'
+import { Todo } from '../../@types/todo.type'
+import styles from './taskInput.module.scss'
+import PropTypes from 'prop-types'
+import { TodoTypes } from '../../PropTypes/todo.prototype'
 
-// interface TaskInputProps {
-//   addTodo: (name: string) => void
-//   editTodo: (name: string) => void
-//   finishEditTodo: () => void
-//   currentTodo: Todo | null
-// }
+interface TaskInputProps {
+  addTodo: (name: string) => void
+  editTodo: (name: string) => void
+  finishEditTodo: () => void
+  currentTodo: Todo | null
+}
 
-// export default function TaskInput(props: TaskInputProps) {
-//   const { addTodo, currentTodo, editTodo, finishEditTodo } = props
-//   const [name, setName] = useState<string>('')
+export default function TaskInput(props: TaskInputProps) {
+  const { addTodo, currentTodo, editTodo, finishEditTodo } = props
+  const [name, setName] = useState<string>('')
 
-//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault()
-//     if (currentTodo) {
-//       finishEditTodo()
-//       if (name) setName('')
-//     } else {
-//       addTodo(name)
-//       setName('')
-//     }
-//   }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    //submit rồi thì name sẽ ko có nữa
+    if (currentTodo) {
+      finishEditTodo()
+      if (name) setName('')
+    } else {
+      addTodo(name)
+      setName('')
+    }
+  }
 
-//   const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     const { value } = event.target
-//     if (currentTodo) {
-//       editTodo(value)
-//     } else {
-//       setName(value)
-//     }
-//   }
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    if (currentTodo) {
+      editTodo(value)
+    } else {
+      setName(value)
+    }
+  }
 
-//   return (
-//     <div className='mb-2'>
-//       <h1 className={styles.title}>To do list typescript</h1>
-//       <form className={styles.form} onSubmit={handleSubmit}>
-//         <input
-//           type='text'
-//           placeholder='caption goes here'
-//           value={currentTodo ? currentTodo.name : name}
-//           onChange={onChangeInput}
-//         />
-//         <button type='submit'>{currentTodo ? '✔️' : '➕'}</button>
-//       </form>
-//     </div>
-//   )
-// }
+  return (
+    <div className='mb-2'>
+      <h1 className={styles.title}>Todo list typescript</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='caption goes here'
+          value={currentTodo ? currentTodo.name : name}
+          onChange={onChangeInput}
+        />
+        <button type='submit'>{currentTodo ? '✔️' : '➕'}</button>
+      </form>
+    </div>
+  )
+}
 
-// TaskInput.propTypes = {
-//   addTodo: PropTypes.func.isRequired,
-//   editTodo: PropTypes.func.isRequired,
-//   finishEditTodo: PropTypes.func.isRequired,
-//   currentTodo: PropTypes.oneOfType([TodoTypes, PropTypes.oneOf([null])])
-// }
+TaskInput.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  editTodo: PropTypes.func.isRequired,
+  finishEditTodo: PropTypes.func.isRequired,
+  currentTodo: PropTypes.oneOfType([TodoTypes, PropTypes.oneOf([null])])
+}
